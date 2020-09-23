9cea25fdd49548a29461bbb8d08ca6828aad5bfa
Mondo <mondo@imondo.cn>
2020-09-23 11:56:18 +0800

    docs: 更新描述

--- docs/index.jsx
+++ docs/index.jsx
@@ -6,10 +6,10 @@ banner:
     - { name: '开 始', href: './documents/triangle.html', primary: true }
     - { name: 'Github >', href: 'https://github.com/one-pupil/css-libs' }
 features: 
-  - { name: '优雅', desc: '经过精雕细琢，我们带给大家一个精心设计的、拥有卓越的视觉与交互体验的文档构建工具' }
-  - { name: '灵动', desc: '我们拥有非常灵活的 插件机制 与 主题定制 功能，正在努力构建活跃的插件社区。也许初次使用未见其惊艳，但当你灵活使用插件后便会发现她的强大' }
-  - { name: '简洁', desc: '以 ‘无形’ 代替 ‘有形’，从开发体验到用户界面的呈现，不断去除冗余的设计，使用户专注于写作与阅读' }
-  - { name: '开源', desc: '作为开源项目，我们拥有高质量的代码、完善的自动化测试流程，对社区的需求能够作出积极快速响应' }
+  - { name: '记录', desc: '记录工作中使用，碰到过的 css 效果' }
+  - { name: '收集', desc: '收集博客中看到的各种牛逼的 css 特效' }
+  - { name: '查阅', desc: '通过记录与收集，方便以后能够随时查阅' }
+  - { name: '学习', desc: '对 css 有更多深入的探索' }
 
 footer:
   copyRight:
